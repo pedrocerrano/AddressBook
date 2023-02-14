b6016195d5048c7d7439d9737a780191b5a78a51
pedrocerrano <82617951+pedrocerrano@users.noreply.github.com>
2023-02-14 16:45:44 -0600

    Adds isFavorites toggle functionality to custom cell.

--- AddressBook/AddressBook/View Controllers/Custom Views/PeopleTableViewCell.swift
+++ AddressBook/AddressBook/View Controllers/Custom Views/PeopleTableViewCell.swift
@@ -9,6 +9,33 @@ import UIKit
 
 class PeopleTableViewCell: UITableViewCell {
 
+    //MARK: - OUTLETS
+    @IBOutlet weak var personNameLabel: UILabel!
+    @IBOutlet weak var personIsFavoriteButton: UIButton!
     
+    
+    //MARK: - PROPERTIES
+    var person: Person? {
+        didSet {
+            udpateViews()
+        } //: PROPERTY OBSERVER
+    } //: COMPUTED
+    
+    
+    //MARK: - HELPER FUNCTIONS
+    func udpateViews() {
+        guard let person = person else { return }
+        personNameLabel.text    = person.name
+        
+        let favoriteImageName   = person.isFavorites ? "star.fill" : "star"
+        let favoriteImage       = UIImage(systemName: favoriteImageName)
+        personIsFavoriteButton.setImage(favoriteImage, for: .normal)
+    }  //: UPDATE VIEWS
+    
+    
+    //MARK: - ACTIONS
+    @IBAction func personFavoriteButtonTapped(_ sender: Any) {
+        
+    } //: FAVORITE TAPPED
     
 } //: CLASS
--- AddressBook/AddressBook/View Controllers/PeopleListTableViewController.swift
+++ AddressBook/AddressBook/View Controllers/PeopleListTableViewController.swift
@@ -13,6 +13,7 @@ class PeopleListTableViewController: UITableViewController {
     @IBOutlet weak var groupNameTextField: UITextField!
     
     
+    
     //MARK: - PROPERTIES
     var groupReceiver: Group?
     
